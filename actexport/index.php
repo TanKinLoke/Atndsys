2dcad815607b9c93e3c2987039da5d8ef21e9d72
kinloketan@gmail.com <kinloketan@gmail.com>
2019-03-21 18:40:29 +0800

    datalist added for Actexport

--- actexport/index.php
+++ actexport/index.php
@@ -28,7 +28,38 @@
                 <div class="content-box-a-center">
                     <div class="activity-selector-box">
                         ACTIVITY NAME
-                        <select class="activity-selector-input" id="activity-selector-input"></select>
+                        <select class="activity-selector-input" id="activity-selector-input">
+                        <?php
+                                $servername="localhost";
+                                $username="root";
+                                $password="";
+                                $dbname="i-Attendance";
+
+                                //Create a connection
+                                $conn = new mysqli($servername,$username,$password,$dbname);
+
+                                //Check if the connection is successful or not
+                                if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                                }
+
+                                $sql = "SELECT * FROM activity_record ORDER BY pkey DESC";
+                                $result = mysqli_query($conn,$sql);
+
+                                if ($result->num_rows > 0) {
+                                    // output data of each row
+                                    while($row = $result->fetch_assoc()) {
+                                        //Datalist for dropdown menu
+                                        echo "<option value=\"".$row["Activity_Name"]."\">".$row["Activity_Name"]."</option>";
+                                    }
+                                } else {
+                                    echo "<option value=\"No result\">No result</option>";
+                                }
+
+                                mysqli_close($conn);
+                                echo "<br>";
+                            ?>
+                        </select>
                     </div>
                     <div class="activity-export-box">
                         <button class="export-button">EXPORT</button>

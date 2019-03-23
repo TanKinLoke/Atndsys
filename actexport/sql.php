17a1137a600f58204e0233bb64fe4147032e3718
kinloketan@gmail.com <kinloketan@gmail.com>
2019-03-23 14:59:17 +0800

    Comment for actexport done

--- actexport/sql.php
+++ actexport/sql.php
@@ -16,11 +16,14 @@
             die("Connection failed: " . $conn->connect_error);  
         }
 
+        //Select everything from the table
         $sql = "SELECT * FROM activity_attendance WHERE Activity_Name='$Actname'";
         $result = mysqli_query($conn,$sql);
 
+        //Add activity name into response
         $response = "$Actname:";
 
+        //Loop through the result
         if (mysqli_num_rows($result) > 0) {
             while($row = mysqli_fetch_assoc($result)) {
                 $value1 = $row['Student_Name'];
@@ -28,12 +31,14 @@
                 $value3 = $row['Class'];
                 $response .= "$value1|$value2|$value3,";
             }
+            //Remove , from the last character in response
             $response = rtrim($response,",");
 
         } else {
             
         }
 
+        //Return the response
         echo $response;
 
     ?>

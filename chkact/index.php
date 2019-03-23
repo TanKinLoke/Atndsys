1b849a8a6cab15286b84dff38207293ab8c51c47
genesis331 <cheah3838@gmail.com>
2019-03-23 14:26:03 +0800

    Frontend Feature Update
    1) Initiated Check Activity Page for backend connection
    
    Signed-off-by: genesis331 <cheah3838@gmail.com>

---
+++ chkact/index.php
@@ -0,0 +1,42 @@
+<html lang="en">
+    <head>
+        <head>
+            <title>ATNDSYS Check Activity</title>
+            <meta name="viewport" content="width=device-width,initial-scale=1.0" initial-scale="1"/>
+            <meta charset="utf-8"/>
+            <link rel="icon" href="../img/favicon.png">
+            <link rel="stylesheet" href="../css/global.css"/>
+            <link rel="stylesheet" href="../css/chkact.css"/>
+        </head>
+    </head>
+    <body>
+        <a class="back-btn" onclick="location.href='../'">
+            <img src="../img/back-arrow.svg">
+        </a>
+        <div class="header">
+            <div class="header-content-box">
+                <div class="header-image-box">
+                    <img src="../img/i-Creatorz-Logo-Black.png" draggable="false"/>
+                </div>
+                <div class="header-project-name-box">
+                    ATTENDANCE SYSTEM
+                </div>
+                <div class="header-page-name-box">
+                    CHECK ACTIVITY
+                </div>
+            </div>
+        </div>
+        <div class="content-box">
+            <div class="content-box-center">
+                <div class="select-activity-box">
+                    ACTIVITY NAME
+                    <select class="activity-selector-input"></select>
+                </div>
+                <div class="confirm-selection-box">
+                    <button class="confirm-selection-input">CHECK</button>
+                </div>
+            </div>
+        </div>
+        <script src=""></script>
+    </body>
+</html>

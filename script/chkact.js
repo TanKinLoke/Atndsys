59b9e8c2b19c6320252004c3c961c6f9f6e374a0
genesis331 <cheah3838@gmail.com>
2019-04-02 21:49:40 +0800

    Frontend Feature Update
    1) Added options to search based on more query types
    Waiting for work to be done in backend
    
    Signed-off-by: genesis331 <cheah3838@gmail.com>

--- script/chkact.js
+++ script/chkact.js
@@ -2,3 +2,8 @@ function displayData() {
     document.getElementById("content-box-a").style.display = "none";
     document.getElementById("content-box-b").style.display = "block";
 }
+
+function getQueryType() {
+    //Get user selected value on search query type
+    return document.getElementById("query-type-selector-input").value;
+}

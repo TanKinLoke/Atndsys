1f643e15f105d67a0b33d261dd3e860f0c934d67
genesis331 <cheah3838@gmail.com>
2019-04-03 17:25:11 +0800

    Frontend Patch Update
    1) Styled table in Check Activity Page
    
    Signed-off-by: genesis331 <cheah3838@gmail.com>

--- script/chkact.js
+++ script/chkact.js
@@ -70,7 +70,7 @@ function getActivity(page) {
                         "<td><input type='date' id='"+activityArray2[0].split(" ").join("_").split("\'").join("-")+"_date_text' value='"+activityArray2[2]+"' readonly=\"true\"></td>\n"+
                         "<td><input type='time' id='"+activityArray2[0].split(" ").join("_").split("\'").join("-")+"_starttime_text' value='"+activityArray2[3]+"' readonly=\"true\"></td>\n"+
                         "<td><input type='time' id='"+activityArray2[0].split(" ").join("_").split("\'").join("-")+"_endtime_text' value='"+activityArray2[4]+"' readonly=\"true\"></td>\n"+
-                        "<td><button type='button' id='"+activityArray2[0].split(" ").join("_").split("\'").join("-")+"_edit' onclick='editActivity(\""+activityArray2[0].split(" ").join("_").split("\'").join("-")+"\")'>Edit</button>\n<button type='button' id='"+activityArray2[0].split(" ").join("_").split("\'").join("-")+"_delete' onclick='deleteActivity(\""+activityArray2[0].split(" ").join("_").split("\'").join("-")+"\")'>Delete</button></td>\n"+
+                        "<td><button type='button' id='"+activityArray2[0].split(" ").join("_").split("\'").join("-")+"_edit' class='edit-btn' onclick='editActivity(\""+activityArray2[0].split(" ").join("_").split("\'").join("-")+"\")'></button>\n<button type='button' id='"+activityArray2[0].split(" ").join("_").split("\'").join("-")+"_delete' class='delete-btn' onclick='deleteActivity(\""+activityArray2[0].split(" ").join("_").split("\'").join("-")+"\")'></button></td>\n"+
                         "</tr>\n"
                     )
             };
@@ -160,7 +160,7 @@ function editActivity(name) {
     $("#"+name+"_starttime_text").attr("readonly",false);
     $("#"+name+"_date_text").attr("readonly",false);
     $("#"+name+"_edit").attr("onclick","doneEditActivity('"+name+"')");
-    $("#"+name+"_edit").text("Done");
+    $("#"+name+"_edit").addClass("toDone");
 }
 
 function doneEditActivity(name) {
@@ -180,7 +180,7 @@ function doneEditActivity(name) {
     $("#"+name+"_starttime_text").attr("readonly",true);
     $("#"+name+"_date_text").attr("readonly",true);
     $("#"+name+"_edit").attr("onclick","editActivity('"+name+"')");
-    $("#"+name+"_edit").text("Edit");
+    $("#"+name+"_edit").removeClass("toDone");
 }
 
 $(document).keypress(function(e) {
